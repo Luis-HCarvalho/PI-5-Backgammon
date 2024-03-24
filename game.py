2a699edf10e6a734d04932b5def196421cffb4c2
Luis-HCarvalho <luishenriquecm12@gmail.com>
2024-03-24 17:58:43 -0300

    feat: add 'game' module, with the implementation of classes 'Player' and 'Game'

---
+++ game.py
@@ -0,0 +1,23 @@
+class Player:
+    def opposite(self):
+        raise NotImplementedError("Must be implemented")
+
+class Game():
+    def turn(self):
+        pass
+
+    def play(self, localizacao):
+        pass
+
+    def valid_moves(self):
+        pass
+
+    def won(self):
+        pass
+
+    def draw(self):
+        return (not self.won()) and (len(self.valid_moves()) == 0)
+
+    def evaluate(self, player):
+        pass
+
